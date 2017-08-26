30f57206ad42fdb557b956225703c211c359d4d5
Eason Gao <easongao1998@gmail.com>
2017-08-26 18:38:39 -0400

    push before it breaks

--- templates/javascript.js
+++ templates/javascript.js
@@ -20,24 +20,28 @@ for (var i = 0; i < people['accountsPayable'].length; i++)
   var tempNode = document.querySelector("div[data-type='template']").cloneNode(true); //true for deep clone
   tempNode.querySelector(".name").textContent = people['accountsPayable'][i]['Name'];
 
+  for (var j = 0; j < people['accountsPayable'][i]['Transactions'].length; j++) {
 
+    // var li = document.createElement('li'); //true for deep clone
+    // li.innerText = "Money Owed: $" + people['accountsPayable'][i]['Transactions'][j]['Amount'];
+    // tempNode.appendChild(li);
 
-      //tempNode.querySelector(".money-owed").textContent = "Money Owed: $" + people['accountsPayable'][i]['Transactions'].
+    var tempNodeList = document.querySelector("div[data-type='listTemplate']").cloneNode(true); //true for deep clone
+    tempNodeList.querySelector(".money-owed-list").textContent = "Money Owed: $" + people['accountsPayable'][i]['Transactions'][j]['Amount'];
+
+    tempNodeList.querySelector(".date").textContent = "Date: " + people['accountsPayable'][i]['Transactions'][j]['Date'];
+
+    //document.body.appendChild(tempNodeList);
+    tempNodeList.style.display = "block";
+    tempNode.appendChild(tempNodeList);
+  }
+
+  //tempNode.querySelector(".money-owed").textContent = "Money Owed: $" + people['accountsPayable'][i]['Transactions'].
   // tempNode.querySelector("img").src = movies[keys[i]].imageurl;
   // tempNode.querySelector("button").onclick = window[movies[keys[i]].func];
   // tempNode.querySelector("a").href = movies[keys[i]].details;
   tempNode.style.display = "block";
   document.body.appendChild(tempNode);
-
-  for (var j = 0; j < people['accountsPayable'][i]['Transactions'].length; j++) {
-
-    var tempNodeList = document.querySelector("li[data-type='template']").cloneNode(true); //true for deep clone
-    tempNodeList.querySelector(".money-owed-list").textContent = "Money Owed: $" + people['accountsPayable'][i]['Transactions'][j]['Amount'];
-
-    tempNodeList.querySelector(".date").textContent = "Date: " + people['accountsPayable'][i]['Transactions'][j]['Date'];
-
-    document.body.appendChild(tempNodeList);
-  }
 
 }
 

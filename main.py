a5cd106f84f6a7eacad18ad5c850f1fec5ce2e90
Nicholas Vadivelu <nick@bassoft.com>
2017-08-26 19:12:25 -0400

    get function works

--- main.py
+++ main.py
@@ -9,12 +9,46 @@ app.config['MONGO_URI'] = 'mongodb://nick:nick@ds161503.mlab.com:61503/hackthe6i
 
 mongo = PyMongo(app)
 
+
+
 @app.route('/')
 def hello():
-    users = mongo.db.usertest
-    users.insert({'name': 'test'})
     return render_template('index.html')
-   
+
+@app.route('/get')
+def get():
+    users = mongo.db.usertest 
+    tester = users.find_one({'_id':'testid'})
+    return str(tester)
+
+@app.route('/create')
+def create():
+    users = mongo.db.usertest
+    users.insert({
+        '_id': 'testid',
+        'accountsPayable': {
+            'name': 'Eason',
+            'total': '-200',
+            'transactions' : [
+                {
+                    'date': '01/01/2001',
+                    'amount': '-900',
+                    'reason': 'potato'
+                },
+                {
+                    'date': '01/02/2001',
+                    'amount': '600',
+                    'reason': 'tomato'
+                },
+                {
+                    'date': '01/03/2001',
+                    'amount': '100',
+                    'reason': 'lmao'
+                },
+            ]
+        }
+    });
+    return 'nice'
 
 if __name__ == "__main__":
     app.secret_key = 'secretkey'

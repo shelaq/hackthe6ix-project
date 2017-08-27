c9e3ec218b73e198ed65c7d19dcec00f7e1b991f
Nicholas Vadivelu <nick@bassoft.com>
2017-08-26 21:10:04 -0400

    soemthing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -13,40 +13,86 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def hello():
+    session['id'] = 'testid'
     return render_template('index.html')
 
 @app.route('/get')
 def get():
     users = mongo.db.usertest 
-    tester = users.find_one({'_id':'testid'})
+    tester = users.find_one({'_id':session['id']})
     return str(tester)
+
+@app.route('/post')
+def post():
+    placeholder = {
+        "name": "Eason",
+        "amount": "300",
+        "theyOweYou": "True",
+        "date": "02/14/2001",
+        "reason": "potato"
+    }
+    
+    amount = int(placeholder['amount'])
+    if not bool(placeholder['theyOweYou']):
+        amount = -1*amount
+    
+
+    users = mongo.db.usertest 
+    tester = users.find_one({'_id':session['id'], 'accountsPayable.name':placeholder['name']})
+
+    if tester:
+        total = amount
+        for i in range(len(tester['accountsPayable'])):
+            total += int(tester['accountsPayable'][i]['total'])
+        
+        users.update({'id':session['id'], "accountsPayable.name":placeholder['name']}, {'$push':{"accountsPayable.$.transactions":{'date':placeholder['date'], 'amount':placeholder['amount'], 'reason':placeholder['reason']}}} )
+        return 'fcn worked'    
+    #else:
+
+    #SHELA ADD CODE TO REPLACE 
+    
+    return str(tester)
+
 
 @app.route('/create')
 def create():
     users = mongo.db.usertest
     users.insert({
         '_id': 'testid',
-        'accountsPayable': {
-            'name': 'Eason',
-            'total': '-200',
-            'transactions' : [
-                {
-                    'date': '01/01/2001',
-                    'amount': '-900',
-                    'reason': 'potato'
-                },
-                {
-                    'date': '01/02/2001',
-                    'amount': '600',
-                    'reason': 'tomato'
-                },
-                {
-                    'date': '01/03/2001',
-                    'amount': '100',
-                    'reason': 'lmao'
-                },
-            ]
-        }
+        'accountsPayable': [
+            {
+                'name': 'Eason',
+                'total': '-300',
+                'transactions' : [
+                    {
+                        'date': '01/01/2001',
+                        'amount': '-900',
+                        'reason': 'potato'
+                    },
+                    {
+                        'date': '01/02/2001',
+                        'amount': '600',
+                        'reason': 'tomato'
+                    }
+                ]
+            },
+            {
+                'name': 'Shela',
+                'total': '100',
+                'transactions' : [
+                    {
+                        'date': '03/03/2003',
+                        'amount': '200',
+                        'reason': 'potato3'
+                    },
+                    {
+                        'date': '04/04/2004',
+                        'amount': '-100',
+                        'reason': 'tomato4'
+                    }
+                ]
+            }
+        ]
     });
     return 'nice'
 
